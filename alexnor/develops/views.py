a4f5bb9e8a0961e3c99df32b9b4953fe3af71059
Alex Nor <advance114@gmail.com>
2024-09-06 14:52:04 +0300

    Working on a template

--- alexnor/develops/views.py
+++ alexnor/develops/views.py
@@ -5,9 +5,17 @@ from django.shortcuts import render, redirect
 
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
-        {'title': "Добавить статью", 'url_name': 'add_page'},
         {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
         {'title': "Войти", 'url_name': 'login'}
+]
+
+cats_db = [
+    {'id': 1, 'name': 'Web develops'},
+    {'id': 2, 'name': 'Games develops'},
+    {'id': 3, 'name': 'Linux'},
+    {'id': 4, 'name': 'Docker'},
+    {'id': 5, 'name': 'Git'},
 ]
 
 
@@ -15,6 +23,7 @@ def index(request):
     data = {
         'title': 'Главная страница',
         'menu': menu,
+        'cat_selected': 0,
     }
     return render(request, 'develops/index.html', context=data)
 
@@ -33,6 +42,15 @@ def contact(request):
 
 def login(request):
     return HttpResponse("Авторизация")
+
+
+def show_category(request, cat_id):
+    data = {
+        'title': 'Отображение по рубрикам',
+        'menu': menu,
+        'cat_selected': cat_id,
+    }
+    return render(request, 'develops/index.html', context=data)
 
 
 
