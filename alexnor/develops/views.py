279b90bd1a59d098af7f410f639bcb29657785b4
Alex Nor <advance114@gmail.com>
2024-10-15 23:29:20 +0300

    Displaying posts by category

--- alexnor/develops/views.py
+++ alexnor/develops/views.py
@@ -1,24 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.urls import reverse
 from django.template.loader import render_to_string
 from django.shortcuts import render, redirect, get_object_or_404
 
-from .models import Alexnor
+from .models import Alexnor, Category
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Обратная связь", 'url_name': 'contact'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
         {'title': "Войти", 'url_name': 'login'}
-]
+        ]
 
-cats_db = [
-    {'id': 1, 'name': 'Web develops'},
-    {'id': 2, 'name': 'Games develops'},
-    {'id': 3, 'name': 'Linux'},
-    {'id': 4, 'name': 'Docker'},
-    {'id': 5, 'name': 'Git'},
-    {'id': 6, 'name': 'News blog'},
-]
+
+# cats_db = [
+#     {'id': 1, 'name': 'Web develops'},
+#     {'id': 2, 'name': 'Games develops'},
+#     {'id': 3, 'name': 'Linux'},
+#     {'id': 4, 'name': 'Docker'},
+#     {'id': 5, 'name': 'Git'},
+#     {'id': 6, 'name': 'News blog'},
+# ]
 
 
 def index(request):
@@ -33,7 +34,7 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'develops/about.html',{'title': 'О сайте', 'menu': menu})
+    return render(request, 'develops/about.html', {'title': 'О сайте', 'menu': menu})
 
 
 def show_post(request, post_slug):
@@ -60,14 +61,17 @@ def login(request):
     return HttpResponse("Авторизация")
 
 
-def show_category(request, cat_id):
+def show_category(request, cat_slug):
+    category = get_object_or_404(Category, slug=cat_slug)
+    posts = Alexnor.published.filter(cat_id=category.pk)
     data = {
-        'title': 'Отображение по рубрикам',
+        'title': f'Рубрика: {category.name}',
         'menu': menu,
-        'posts': Alexnor.published.all(),
-        'cat_selected': cat_id,
+        'posts': posts,
+        'cat_selected': category.pk,
     }
     return render(request, 'develops/index.html', context=data)
 
 
-
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
