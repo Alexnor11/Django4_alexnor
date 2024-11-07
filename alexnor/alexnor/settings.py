a1198d547e4dd49bd526483d7a311ffe59664baa
Alex Nor <advance114@gmail.com>
2024-11-07 20:51:59 +0300

    authorization and registration

--- alexnor/alexnor/settings.py
+++ alexnor/alexnor/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 
     'django_extensions',
     'develops.apps.DevelopConfig',
+    'users.apps.UsersConfig'
 
 ]
 
--- alexnor/develops/views.py
+++ alexnor/develops/views.py
@@ -1,120 +1,35 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.template.loader import render_to_string
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, FormView, CreateView
 
 from .forms import AddPostForm, UploadFileForm
 from .models import Alexnor, Category, UploadFiles
-
-menu = [{'title': "О сайте", 'url_name': 'about'},
-        {'title': "Обратная связь", 'url_name': 'contact'},
-        {'title': "Добавить статью", 'url_name': 'add_page'},
-        {'title': "Войти", 'url_name': 'login'}
-        ]
+from .utils import DataMixin
 
 
-def index(request):
-    data = {
-        'title': 'Главная страница',
-        'menu': menu,
-    }
-    return render(request, 'develops/index.html', context=data)
+class AlexIndex(DataMixin, ListView):
+    queryset = Alexnor.published.all().select_related('cat')
+    template_name = 'develops/index.html'
+    context_object_name = 'posts'
 
 
-class AlexBlog(ListView):
+class AlexBlog(DataMixin, ListView):
     queryset = Alexnor.published.all().select_related('cat')
     template_name = 'develops/blog.html'
     context_object_name = 'posts'
-    extra_context = {
-        'title': 'Главная страница',
-        'menu': menu,
-        'cat_selected': 0,
-    }
-
-    # def get_queryset(self):
-    #     return Alexnor.published.all().select_related('cat')
-
-
-# class AlexBlog(TemplateView):
-#     template_name = 'develops/blog.html'
-#     extra_context = {
-#         'title': 'Главная страница',
-#         'menu': menu,
-#         'posts': Alexnor.published.all(),
-#         'cat_selected': 0,
-#     }
-
-
-# def blog(request):
-#     posts = Alexnor.published.all()
-#     data = {
-#         'title': 'Главная страница',
-#         'menu': menu,
-#         'posts': posts,
-#     }
-#
-#     return render(request, 'develops/blog.html', context=data)
 
 
 def about(request):
     return render(request, 'develops/about.html',
-                  {'title': 'О сайте', 'menu': menu})
-
-
-# def show_post(request, post_slug):
-#     post = get_object_or_404(Alexnor, slug=post_slug)
-#
-#     data = {
-#         'title': post.title,
-#         'menu': menu,
-#         'post': post,
-#         'cat_selected': 1,
-#     }
-#     return render(request, 'develops/post.html', context=data)
-
-
-class ShowPost(DetailView):
-    model = Alexnor
-    template_name = 'develops/post.html'
-    slug_url_kwarg = 'post_slug'
-    context_object_name = 'post'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = context['post'].title
-        context['menu'] = menu
-        return context
-
-    def get_object(self, queryset=None):
-        return get_object_or_404(Alexnor.published, slug=self.kwargs[self.slug_url_kwarg])
-
-
-class AddPage(View):
-    pass
-
-
-def addpage(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = AddPostForm()
-
-    data = {
-        'menu': menu,
-        'title': 'Добавление статьи',
-        'form': form
-    }
-    return render(request, 'develops/addpage.html', context=data)
+                  {'title': 'О сайте',})
 
 
 def contact(request):
     data = {
-        'menu': menu,
+        # 'menu': menu,
         'title': 'Форма обратной связи'
     }
     return render(request, 'develops/contact.html', context=data)
@@ -124,33 +39,41 @@ def login(request):
     return HttpResponse("Авторизация")
 
 
-class AlexCategory(ListView):
+class ShowPost(DataMixin, DetailView):
+    model = Alexnor
+    template_name = 'develops/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return self.get_mixin_context(context, title=context['post'].title)
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Alexnor.published, slug=self.kwargs[self.slug_url_kwarg])
+
+
+class AddPage(DataMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'develops/addpage.html'
+    title_page = 'Добавление статьи'
+
+
+class AlexCategory(DataMixin, ListView):
     template_name = 'develops/blog.html'
     context_object_name = 'posts'
     allow_empty = False
 
-    def get_context_data(self, *, object_list=None, **kwargs):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         cat = context['posts'][0].cat
-        context['title'] = 'Категория - ' + cat.name
-        context['menu'] = menu
-        context['cat_selected'] = cat.id
-        return context
+        return self.get_mixin_context(context,
+                                      title='Категория - ' + cat.name,
+                                      cat_selected=cat.pk,
+                                      )
 
     def get_queryset(self):
         return Alexnor.published.filter(cat__slug=self.kwargs['cat_slug']).select_related('cat')
-
-
-# def show_category(request, cat_slug):
-#     category = get_object_or_404(Category, slug=cat_slug)
-#     posts = Alexnor.published.filter(cat_id=category.pk)
-#     data = {
-#         'title': f'Рубрика: {category.name}',
-#         'menu': menu,
-#         'posts': posts,
-#         'cat_selected': category.pk,
-#     }
-#     return render(request, 'develops/blog.html', context=data)
 
 
 def page_not_found(request, exception):
